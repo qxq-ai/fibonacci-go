b0ca630138898829524c94773479575b3acd2040
David Lewis <david@exylys.mx>
2024-07-25 13:20:42 -0600

    Basic web aseembly html loader.

---
+++ renderHtml/api.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"syscall/js"
+)
+
+func main() {
+	c := make(chan struct{}, 0)
+
+	// Exponer la función sayHello al JavaScript
+	js.Global().Set("sayHello", js.FuncOf(sayHello))
+
+	<-c // Evitar que la función main termine
+}
+
+func sayHello(this js.Value, args []js.Value) interface{} {
+	return js.ValueOf("layout.html")
+}
---
+++ renderHtml/go.mod
@@ -0,0 +1,3 @@
+module renderHtml
+
+go 1.22.5
